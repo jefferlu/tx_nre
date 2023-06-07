30771604d4dab64fe99014d6139328290b634e23
jeffer <id8042@gmail.com>
2023-06-07 22:21:52 +0800

    update commit

--- backend/tx_nre/urls.py
+++ backend/tx_nre/urls.py
@@ -21,8 +21,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    
-    path('', TemplateView.as_view(template_name='index.html')),
+
+    # path('', TemplateView.as_view(template_name='index.html')),
 ]
 
 # urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'), name='index'), ]
