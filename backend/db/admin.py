0774d00a968734e86aeb20807f9392ab94ef357f
呂建鋒 <jeffer@lvjianfengdeMacBook-Pro.local>
2023-07-04 23:54:25 +0800

    update commit

--- backend/db/admin.py
+++ backend/db/admin.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'power_ratio',)
-
-
 @admin.register(models.Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('no', 'name', 'man_working_hours', 'equip_working_hours', )
@@ -28,6 +23,12 @@ class TestItemAdmin(admin.ModelAdmin):
     list_display = ('function', 'item', 'lab_location',)
 
 
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'customer', 'power_ratio',)
+
+
 @admin.register(models.Record)
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('test_item', 'project', 'concept_test_uut', 'concept_need_test', 'concept_regression_rate',)
+    list_display = ('test_item', 'project', 'concept_test_uut',
+                    'concept_need_test', 'concept_regression_rate',)
--- backend/db/models.py
+++ backend/db/models.py
@@ -1,22 +1,13 @@
 from django.db import models
 
 
-class Project(models.Model):
-    name = models.CharField(max_length=120, verbose_name='name')
-    power_ratio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True,  verbose_name='power ratio')
-
-    class Meta:
-        db_table = 'nre_project'
-
-    def __str__(self):
-        return self.name
-
-
 class Item(models.Model):
     no = models.CharField(max_length=120, verbose_name='no')
     name = models.CharField(max_length=120, verbose_name='name')
-    equip_working_hours = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='equip_working_hours')
-    man_working_hours = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='man_working_hours')
+    equip_working_hours = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name='equip_working_hours')
+    man_working_hours = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name='man_working_hours')
 
     class Meta:
         db_table = 'nre_item'
@@ -36,7 +27,8 @@ class Customer(models.Model):
 
 
 class Function(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='customer')
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, verbose_name='customer')
     name = models.CharField(max_length=120, verbose_name='name')
     order = models.IntegerField(null=True, blank=True)
 
@@ -53,9 +45,12 @@ class TestItem(models.Model):
         (1, 'Outsourcing')
     ]
 
-    function = models.ForeignKey(Function, on_delete=models.CASCADE, related_name='test_items', verbose_name='function')
-    item = models.OneToOneField(Item, on_delete=models.CASCADE, verbose_name='item')
-    lab_location = models.IntegerField(default=1, choices=LAB_LOCATION, verbose_name='lab_location')
+    function = models.ForeignKey(
+        Function, on_delete=models.CASCADE, related_name='test_items', verbose_name='function')
+    item = models.OneToOneField(
+        Item, on_delete=models.CASCADE, verbose_name='item')
+    lab_location = models.IntegerField(
+        default=1, choices=LAB_LOCATION, verbose_name='lab_location')
 
     class Meta:
         db_table = 'nre_test_item'
@@ -66,7 +61,8 @@ class TestItem(models.Model):
 
 
 class Fee(models.Model):
-    test_item = models.ForeignKey(TestItem, on_delete=models.CASCADE, related_name='quotations', verbose_name='test_item')
+    test_item = models.ForeignKey(
+        TestItem, on_delete=models.CASCADE, related_name='quotations', verbose_name='test_item')
     year = models.IntegerField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -74,29 +70,60 @@ class Fee(models.Model):
         db_table = 'nre_fee'
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=120, verbose_name='name')
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, verbose_name='customer')
+    power_ratio = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True,  verbose_name='power ratio')
+
+    class Meta:
+        db_table = 'nre_project'
+
+    def __str__(self):
+        return self.name
+
+
 class Record(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='project', related_name='records', null=True, blank=True)
-    test_item = models.ForeignKey(TestItem, on_delete=models.CASCADE, verbose_name='test_item')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                verbose_name='project', related_name='records', null=True, blank=True)
+    test_item = models.ForeignKey(
+        TestItem, on_delete=models.CASCADE, verbose_name='test_item')
 
-    concept_test_uut = models.IntegerField(null=True, blank=True, verbose_name='concept_test_uut')
-    concept_need_test = models.BooleanField(default=False, verbose_name='concept_need_test')
-    concept_regression_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='concept_regression_rate')
+    concept_test_uut = models.IntegerField(
+        null=True, blank=True, verbose_name='concept_test_uut')
+    concept_need_test = models.BooleanField(
+        default=False, verbose_name='concept_need_test')
+    concept_regression_rate = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='concept_regression_rate')
 
-    bu_test_uut = models.IntegerField(null=True, blank=True, verbose_name='bu_test_uut')
-    bu_need_test = models.BooleanField(default=False, verbose_name='bu_need_test')
-    bu_regression_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='bu_regression_rate')
+    bu_test_uut = models.IntegerField(
+        null=True, blank=True, verbose_name='bu_test_uut')
+    bu_need_test = models.BooleanField(
+        default=False, verbose_name='bu_need_test')
+    bu_regression_rate = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='bu_regression_rate')
 
-    ct_test_uut = models.IntegerField(null=True, blank=True, verbose_name='ct_test_uut')
-    ct_need_test = models.BooleanField(default=False, verbose_name='ct_need_test')
-    ct_regression_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='ct_regression_rate')
+    ct_test_uut = models.IntegerField(
+        null=True, blank=True, verbose_name='ct_test_uut')
+    ct_need_test = models.BooleanField(
+        default=False, verbose_name='ct_need_test')
+    ct_regression_rate = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='ct_regression_rate')
 
-    nt_test_uut = models.IntegerField(null=True, blank=True, verbose_name='nt_test_uut')
-    nt_need_test = models.BooleanField(default=False, verbose_name='nt_need_test')
-    nt_regression_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='nt_regression_rate')
+    nt_test_uut = models.IntegerField(
+        null=True, blank=True, verbose_name='nt_test_uut')
+    nt_need_test = models.BooleanField(
+        default=False, verbose_name='nt_need_test')
+    nt_regression_rate = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='nt_regression_rate')
 
-    ot_test_uut = models.IntegerField(null=True, blank=True, verbose_name='ot_test_uut')
-    ot_need_test = models.BooleanField(default=False, verbose_name='ot_need_test')
-    ot_regression_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='ot_regression_rate')
+    ot_test_uut = models.IntegerField(
+        null=True, blank=True, verbose_name='ot_test_uut')
+    ot_need_test = models.BooleanField(
+        default=False, verbose_name='ot_need_test')
+    ot_regression_rate = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='ot_regression_rate')
 
     class Meta:
         db_table = 'nre_record'
