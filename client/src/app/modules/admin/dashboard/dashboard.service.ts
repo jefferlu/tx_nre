8310feddad5801b56b5077b1f9a41002e50b391e
jeffer <id8042@gmail.com>
2023-10-26 00:03:31 +0800

    update commit

--- client/src/app/modules/admin/dashboard/dashboard.service.ts
+++ client/src/app/modules/admin/dashboard/dashboard.service.ts
@@ -35,13 +35,4 @@ export class DashboardService {
             })
         );
     }
-
-    blobToBase64(blob: Blob): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onload = () => resolve(reader.result as string);
-            reader.onerror = reject;
-            reader.readAsDataURL(blob);
-        });
-    }
 }
